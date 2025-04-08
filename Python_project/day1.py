7bffc20e3951265079c0cc571043f215d032aabf
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-04-09 01:24:42 +0530

    Update day1.py

--- Python_project/day1.py
+++ Python_project/day1.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(R"C:\Users\Welcome\Downloads\airqualtiyindex.csv")
+
+print("First five values: ")
+df.head()
+print("Info of the data: ")
+df.info()
+print("Description of the data: ")
+df.describe()
 
