1b57061a63a79ad77f072a12d448c82bb30a6bde
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-04-09 14:44:05 +0530

    Update day1.py

--- Python_project/day1.py
+++ Python_project/day1.py
@@ -15,5 +15,10 @@ print("# Data Cleaning: \n")
 print("Total number of missing values = ")
 print(df.isnull().sum())
 
+print("Rows with any missing values: ")
+print(df[df.isnull().any(axis=1)])
+print("\n")
 
+df_cleaned = df.dropna()#heer all null values are dropped from dataset
+df_filled = df.fillna(0) #here all null values are replaced by 0
 
